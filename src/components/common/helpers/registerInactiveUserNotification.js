71d4c356d68889b31916c99648a9d0316ee0f0ad
Jakub Najman <jakub@najman.cz>
2024-05-14 14:32:04 +0200

    repair inactive days input

--- src/components/common/helpers/registerInactiveUserNotification.js
+++ src/components/common/helpers/registerInactiveUserNotification.js
@@ -3,7 +3,7 @@ import * as Notifications from 'expo-notifications';
 export const scheduleInactiveUserNotification = async (inactiveDays, notificationsActive) => {
 	await Notifications.cancelAllScheduledNotificationsAsync();
 	if (notificationsActive) {
-		console.log("Scheduling notification");
+		console.log("Scheduling notification for: ", inactiveDays + " days");
 		await Notifications.scheduleNotificationAsync({
 			content: {
 				title: "Time to exercise",
@@ -17,9 +17,27 @@ export const scheduleInactiveUserNotification = async (inactiveDays, notificatio
 	}
 };
 
-
 // Call this after finishing a workout
 export const scheduleNotificationAfterWorkoutFinished = async (inactiveDays, notificationsActive) => {
 	await Notifications.setBadgeCountAsync(0);
 	await scheduleInactiveUserNotification(inactiveDays, notificationsActive);
+}
+
+
+// ONLY for testing purposes
+export const scheduleTestingNotification = async (inactiveDays, notificationActive) => {
+	await Notifications.cancelAllScheduledNotificationsAsync();
+	if (notificationActive) {
+		console.log("Scheduling notification for: ", inactiveDays, " minutes");
+		await Notifications.scheduleNotificationAsync({
+			content: {
+				title: "Time to exercise",
+				body: "Reminding you to start growing muscles!",
+			},
+			trigger: {
+				seconds: inactiveDays * 60, // inactiveDays are number of minutes in testing cases
+				repeats: true,
+			},
+		});
+	}
 }
